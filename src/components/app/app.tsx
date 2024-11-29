71337cc5b73054546af51e3f498139f943f595e9
Павел Корольков <roflgan@gmail.com>
2024-11-29 15:16:07 +0700

    WIP

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,10 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { AppRoute } from '../../const';
 import MainScreen from '../../pages/main-screen/main-screen';
+import FavoritesScreen from '../../pages/favorite-screen/favorite-screen';
+import LoginScreen from '../../pages/login-screen/login-screen';
+import OfferScreen from '../../pages/offer-screen/offer-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
 
 type AppScreenProps = {
   placesToStay: number;
@@ -8,11 +14,30 @@ type AppScreenProps = {
 
 function App({ placesToStay, emailAddress, favoriteCount }: AppScreenProps): JSX.Element {
   return (
-    <MainScreen
-      placesToStay={placesToStay}
-      emailAddress={emailAddress}
-      favoriteCount={favoriteCount}
-    />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainScreen placesToStay={placesToStay} emailAddress={emailAddress} favoriteCount={favoriteCount} />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<FavoritesScreen emailAddress={emailAddress} favoriteCount={favoriteCount} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginScreen />}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<OfferScreen emailAddress={emailAddress} favoriteCount={favoriteCount} />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundScreen />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/not-found-screen/not-found-screen.tsx
@@ -0,0 +1,41 @@
+import { Link } from 'react-router-dom';
+
+function NotFoundScreen(): JSX.Element {
+  return (
+    <div className="page page--gray page--login">
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <Link className="header__logo-link" to='/'>
+                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width={81} height={41} />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </header>
+      <main className="page__main page__main--login">
+        <div className="page__login-container container">
+          <section className="login">
+            <h1 className="login__title">Something`s gone wrong...</h1>
+            <img className="login__title" src="img/thinking-emoji.png" alt="thinking-emoji" width={64} height={64} />
+            <div className="locations__item">
+              <Link className="locations__item-link" to='/'>
+                <span>Go to main page</span>
+              </Link>
+            </div>
+          </section>
+          <section className="locations locations--login locations--current">
+            <div className="locations__item">
+              <div className="locations__item-link">
+                <span>404. Page not found</span>
+              </div>
+            </div>
+          </section>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default NotFoundScreen;
