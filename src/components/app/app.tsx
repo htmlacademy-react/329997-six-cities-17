464aad516c8c3510ec8b2e96a05eb18858a0b0f4
Павел Корольков <roflgan@gmail.com>
2024-12-01 15:06:02 +0700

    3.19. Маршрут по тайным тропам

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,10 +1,11 @@
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
-import { AppRoute } from '../../const';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import MainScreen from '../../pages/main-screen/main-screen';
 import FavoritesScreen from '../../pages/favorite-screen/favorite-screen';
 import LoginScreen from '../../pages/login-screen/login-screen';
 import OfferScreen from '../../pages/offer-screen/offer-screen';
 import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
+import PrivateRoute from '../private-route/private-route';
 
 type AppScreenProps = {
   placesToStay: number;
@@ -22,7 +23,13 @@ function App({ placesToStay, emailAddress, favoriteCount }: AppScreenProps): JSX
         />
         <Route
           path={AppRoute.Favorites}
-          element={<FavoritesScreen emailAddress={emailAddress} favoriteCount={favoriteCount} />}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <FavoritesScreen emailAddress={emailAddress} favoriteCount={favoriteCount} />
+            </PrivateRoute>
+          }
         />
         <Route
           path={AppRoute.Login}
